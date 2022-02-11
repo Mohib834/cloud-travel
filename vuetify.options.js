d9cbc4e6b5a65d89ddbaf97a8c1ff35122483491
Mohib Arshi <mohibarshi834@gmail.com>
2022-02-11 17:30:01 +0530

    feat: project setup done

---
+++ vuetify.options.js
@@ -0,0 +1,32 @@
+// Icons
+
+// Primary
+const colorA100 = '#0A0A0A'
+const colorA200 = '#181818;'
+const colorA300 = '#222224'
+// const colorA400 = '#272727';
+// Secondary
+const colorB100 = '#D1FF4E'
+// const colorB200 = '#A066FF';
+// const colorB300 = '#FF66F9';
+// const colorB400 = '#8DFFC2';
+// const colorB500 = '#124F5D';
+// const colorB600 = '#5E4126';
+// const colorB700 = '#47145D';
+
+export default {
+  theme: {
+    light: true,
+    options: { customProperties: true },
+    themes: {
+      light: {
+        primary: '#002D63',
+        colorA100,
+        colorA200,
+        colorA300,
+        secondary: colorB100,
+        colorB100,
+      },
+    },
+  },
+}
